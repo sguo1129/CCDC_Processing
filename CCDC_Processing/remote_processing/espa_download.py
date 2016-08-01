dde447b4b8ef824be74b0a5e4c35cd39b61769a3
klsmith-usgs <klsmith@usgs.gov>
2016-08-01 07:55:53 -0500

    download work

--- CCDC_Processing/remote_processing/espa_download.py
+++ CCDC_Processing/remote_processing/espa_download.py
@@ -21,17 +21,16 @@ def retrieve_order(output_path, order_id, config_path=None):
     for item in prod_status:
         filename = item['product_dload_url'].split('/')[-1]
         url = host.format(order_id, filename)
+        time.sleep(1)
 
         t = time.time()
-        subprocess.call('wget -c -P {} {}'.format(output_path, url), shell=True)
+        # subprocess.call('wget -c -P {} {}'.format(output_path, url), shell=True)
+
+        resp = requests.get(url, stream=True)
+        if resp.status_code == 200:
+            with open(os.path.join(output_path, filename), 'wb') as f:
+                for chunk in resp.iter_content(chunk_size=2048):
+                    f.write(chunk)
+
         if os.path.exists(os.path.join(output_path, filename)):
             print os.path.getsize(os.path.join(output_path, filename)) / 1024 / (time.time() - t) / 1024
-        #
-        # t = time.time()
-        # resp = requests.get(host.format(order_id, filename), stream=True)
-        # if resp.status_code == 200:
-        #     with open(os.path.join(output_path, filename), 'wb') as f:
-        #         for chunk in resp.iter_content(chunk_size=2048):
-        #             f.write(chunk)
-        #
-        # print os.path.getsize(os.path.join(output_path, filename)) / (time.time() - t) / 1024
